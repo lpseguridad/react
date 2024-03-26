70fea429ee8f16eab2a3eea280144782c28e038e
lpseguridad <infolp.seguridad@gmail.com>
2024-03-26 07:25:19 -0300

    mui_grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Counter from "./components/common/counter/Counter";
-import { Footer } from "./components/layout/footer/Footer";
-import { Navbar } from "./components/layout/navbar/Navbar";
-import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import Mui from "./components/common/mui/Mui";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <ItemListContainer />
-      <Footer />
-      <Counter />
+      <Mui />
     </div>
   );
 }
